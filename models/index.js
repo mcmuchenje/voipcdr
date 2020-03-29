e4bfecf194d47cab633799bbc04f3560dc69706f
mcmuchenje <30975277+mcmuchenje@users.noreply.github.com>
2020-03-29 09:42:10 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const pool  = mysql.createPool({
   connectionLimit : 10,
   host: "192.168.1.201",
-  user: "developer",
-  password: "ch@ng3itDev",
+  user: "admin",
+  password: "somepassword",
   database: "asteriskcdrdb",
   multipleStatements: true
 });
