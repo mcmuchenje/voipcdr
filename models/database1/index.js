590b2f551f0837d9ff021cf0340939554e157751
root <munyac.muchenje@yahoo.co.uk>
2020-12-24 17:10:19 +0200

    moved .env to config

--- models/database1/index.js
+++ models/database1/index.js
@@ -1,4 +1,4 @@
-require ('custom-env').env('cdr', (__dirname + '/../../'))
+require ('custom-env').env('cdr', (__dirname + '/../../config'))
 
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
